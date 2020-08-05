180b718a50031b81972c5307a030ddb67478bbb0
Shyamendra Sengar <55234454+sengarsp@users.noreply.github.com>
2020-08-05 15:44:41 +0530

    Delete main.dart

--- main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-import 'package:flutter/material.dart';
-//import 'package:media_player/ui/home.dart' show MyApp;
-import 'ui/audio.dart';
-main(){
-  runApp(AudioPlayer());
-}
